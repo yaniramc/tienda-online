633eae47a2b5d039a21b67cb7de52be8c22880f6
Yanira Martín Cámara <yanira.martin.camara@gmail.com>
2023-01-16 15:47:34 +0100

    He modificado el diseño de la  página en detalle.

--- frontend/components/ProductCardDetail.js
+++ frontend/components/ProductCardDetail.js
@@ -9,14 +9,13 @@ export default function ProductCardDetail({product}) {
                 <div className="row">
                     <div className="col-sm-12 col-md-6">
                         <h1>{product.name}</h1>
-                        <img src={product.image} alt="image-product" width={500} height={350}/>
-                        <p>{product.description}</p>
-
+                        <img src={product.image} alt="image-product" width={500} height={350} className="my-5"/>
                     </div>
-                    <div className="col-sm-12 col-md-6">
-                        <h1>{product.price}</h1>
-                        <p>{product.stock}</p>
-                        <Link href="/pageShoppingCart" className="btn btn-warning text-white">Añadir a la cesta</Link>
+                    <div className="col-sm-12 col-md-6 my-5">
+                        <h4 className="pt-5 pb-3">Descripción del producto: {product.description}</h4>
+                        <h1 className="text-danger">{product.price} €</h1>
+                        <h4 className="text-success pt-2 pb-5">Stock: {product.stock}</h4>
+                        <Link href="/pageShoppingCart" className="btn btn-warning btn-lg text-white">Añadir a la cesta</Link>
                     </div>
                 </div>
             </div>
